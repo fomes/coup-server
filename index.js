0ecc406c802810d85d7a0269e87067221ee72465
fomes <fgomesdeluna@gmail.com>
2023-10-31 15:19:41 -0300

    Update local origin

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const cors = require("cors");
 const app = express();
 
 app.use((req, res, next) => {
-  // res.setHeader("Access-Control-Allow-Origin", process.env.APP_URL);
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  // res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader("Access-Control-Allow-Origin", process.env.APP_URL);
   res.setHeader(
     "Access-Control-Allow-Methods",
     "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
